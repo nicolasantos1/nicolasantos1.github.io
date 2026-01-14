c4e8cd55e589c0a52b5498944329ba6d2b6ee8c5
Nicolas <nicolasantos011@gmail.com>
2026-01-13 21:32:54 -0300

    version 2

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
-function img()
-{
-    document.getElementById("trocar img").src="img"
-   
+
+
+console.log("JS conectado com sucesso");
+
+function mostrar(secao) {
+  document.querySelectorAll("section").forEach(secao => secao.style.visibility = "collapse");
+    document.querySelectorAll("section").forEach(secao => secao.style.display = "none");
+
+  document.getElementById(secao).style.visibility = "visible";
+  document.getElementById(secao).style.display = "block";
 }
