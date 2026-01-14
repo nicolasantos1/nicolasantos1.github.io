758409c5191f142471b6baa69128ccc9cbec1c92
Nicolas <nicolasantos011@gmail.com>
2026-01-14 17:59:22 -0300

    up

--- js/script.js
+++ js/script.js
@@ -3,15 +3,26 @@
 console.log("JS conectado com sucesso");
 
 function mostrar(secao) {
-  document.querySelectorAll("section").forEach(secao => secao.style.visibility = "collapse");
-  document.querySelectorAll("section").forEach(secao => secao.style.display = "none");
 
-  document.getElementById(secao).style.visibility = "visible";
-  document.getElementById(secao).style.display = "block";
+	document.querySelectorAll("section").forEach(secao => secao.style.display = "none");
+	document.getElementById(secao).style.display = "block";
+	show(secao);
 }
 
-document.getElementById("btnDownload").addEventListener("click", () => {
-    console.log('foii');  
+function show(secao){
+
+	document.querySelectorAll('.lista').forEach( function(element){
+		element.style.display = 'block'
+	} )
+	
+	const item = document.getElementById('nav-' + secao);
+	if (item) {
+		item.style.display = "none";
+	}
+	
+}
+
+document.getElementById("btnDownload").addEventListener("click", () => { 
     const link = document.createElement("a");
     link.href = "date/Currículo - Nicolas Santos Do Nascimento.pdf"; // caminho ou URL do arquivo
     link.download = "Currículo - Nicolas Santos Do Nascimento"; // nome do arquivo
